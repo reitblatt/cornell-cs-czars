7b464c3c027809a27e6c32eb0ac79546111178a1
Mark Reitblatt <reitblatt@cs.cornell.edu>
2012-05-04 10:55:03 -0400

    Added next website dir, info not fully transfered. Still need to figure out CSS to make the tables look pretty

---
+++ test/foot.php
@@ -0,0 +1,20 @@
+</div>
+<div id="copy">
+<div style="margin:0px; padding:0px 0px 10px 0px;float:left; width:180px;">
+<a href="http://validator.w3.org/check?uri=referer"><img 
+  src="http://www.w3.org/Icons/valid-xhtml10-blue" 
+  alt="Validate XHTML" 
+  height="31" 
+  width="88" /></a>
+<a href="http://jigsaw.w3.org/css-validator/check/referer"><img 
+  src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+  alt="Validate CSS" 
+  height="31"
+  width="88" /></a>
+</div>
+<div style="float:left; width:520px; margin:10px auto; text-align:center; ">
+Last updated April 25th, 2012
+</div>
+</div>
+</body>
+</html>
---
+++ test/index.php
@@ -0,0 +1,75 @@
+<?php include("head.php") ?>
+
+<div class="hideable">
+
+<h1>Overview</h1> 
+
+<p>As you might guess, these are not the department&#39;s personal Russian emperors. Rather, a &quot;czar&quot; is a member of the department who is providing a service to the department as a volunteer. 
+
+<br><br>There is no requirement that students act as czars, but the work that czars do helps the department run smoothly and everybody benefits in some way from the czar system, so you are urged to take on czarships occasionally to carry your share of the burden of department work. 
+
+<br><br>If there is a czarship that you think should be created (particularly if you would be willing to take it on yourself at the beginning), contact the Czar Czar (currently Mark Reitblatt, <a class="script" href="javascript:email()">reitblatt</a>) and let him know.</font><font face="Verdana" size=2><br></font></p>
+
+<h1>Current Czars</h1>
+<br/>
+<table class="schedule">
+<tr>
+   <td><b>Title</b></td>
+   <td><b>Czars</b></td>
+</tr>
+<?php
+    czar('Brown Bag', 'Hussam Abu-Libdeh', '');
+    czar('Coke', 'Myle Ott, Ben Sowell', '');
+    czar('Colloquium', 'Guozhang Wang, Nan Rong, Brad Gulko, Chenhao Tan, Daniel Hauagge, Mark Reitblatt', '');
+    czar('Czar Czar', 'Mark Reitblatt', '');
+    czar('Desk', 'Amit Sharma', '');
+    czar('Espresso', 'Daniel Hauagge, Sudip Roy', '');
+    czar('Hockey', 'Alex Fix, Jeff Chadwick, Cristian Danescu-Niculescu-Mizil, Jean-Baptiste Jeannin', '');
+    czar('Mentoring', 'Brad Gulko', '');
+    czar('Milk', 'Nan Rong, Joyce Chen', '');
+    czar('Photo', 'Vasu Raman', '');
+    czar('Picnic', 'Jeff Chadwick', '');
+    czar('Programming Contest', '', '');
+    czar('Refrigerator', 'Ainur Tessenalina', '');
+    czar('Student Brown Bag', 'Mark Reitblatt', '');
+    czar('Talks Website', 'Gabriel Bender, Joyce Chen, Dustin Tseng', '');
+    czar('Visit Day', 'Tim Langlois, Karn Seth, Stavros Nikolaou', '');
+    czar('TGIF', 'Alex Fix', '');
+    czar('Lounge', 'Deniz Altinbucken', '');
+    czar('Systems Lunch', 'Tuan Cao', '');
+?>
+</table>
+<br>
+<h1>Open Czarships</h1>
+<br/>
+<table class="schedule">
+<tr>
+   <td><b>Title</b></td>
+   <td><b>Czars</b></td>
+</tr>
+<?php
+    czar('Frisbee', '', '');
+    czar('Programming Contest', '', '');
+    czar('Quotation', '', '');
+    czar('Foosball', '', '');
+?>
+</table>
+<br>
+<h1>Lapsed Czarships</h1>
+<br/>
+<table class="schedule">
+<tr>
+   <td><b>Title</b></td>
+   <td><b>Czars</b></td>
+</tr>
+<?php
+    czar('Videotaping', '', '');
+    czar('Relocation Czar', '', '');
+    czar('T-Shirt Czar', '', '');
+    czar('Newspaper', '', '');
+    czar('Theater', '', '');
+?>
+</table>
+
+</div>
+<?php include("foot.php") ?>
