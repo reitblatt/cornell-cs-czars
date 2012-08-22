6db17fb9e6513d6a42e115a20e17e28454c30cf8
Mark Reitblatt <reitblatt@cs.cornell.edu>
2012-08-23 00:12:59 +0200

    Added test colloquium page

---
+++ test/colloquium.php
@@ -0,0 +1,27 @@
+<?php include("head.php") ?>
+<div class="hideable">
+<h1>Description</h1>
+
+<p>The Colloquium Czar unlocks the lecture hall for the weekly department colloquium and makes sure that the overhead projectors, microphones, and laser pointers are available and working correctly. After the colloquium, they store away the equipment, and lock the lecture hall. <br><br>In a little more detail, the job involves:<br><br></p>
+
+<ul>
+   <li>Going to the colloquium room about 15 minutes before the colloquium, preferably at the same time as the speaker.<br></li>
+   <li>Setting up the speaker&#39;s laptop.&nbsp; If the projector is not working, try the second one.&nbsp; Failing that, run to get the portable projector. After colloquium, (if there were problems), call technical support to let them know that something needs to be fixed.<br></li>
+   <li>Test the microphone, making sure the batteries have enough charge. <br></li>
+   <li>Mic up the speaker.<br></li>
+   <li>Remember the department laser pointer and offer it to speaker.<br></li>
+   <li>&#91;Afterwards&#93; Turn off the microphone and return to drawer. Turn off projector. Turn off lights. Lock the colloquium room.<br></li>
+</ul>
+
+<h1>Current Czars</h1>
+<ul>
+<li>Guozhang Wang, Nan Rong, Brad Gulko, Chenhao Tan, Daniel Cabrini Hauagge, Mark Reitblatt</li>
+</ul>
+
+<h1>Past Czars</h1>
+<ul>
+<li>Oliver Kennedy,  Benyah Shaparenko, Lucja Kot, Jed Liu, Lonnie Princehouse, Lars Backstrom, Ymir Vigfusson, Art Munson, Mingsheng Hong, Eric Breck, Bo Pang, Yangling Wang, Matthew Harris, Nate Nystrom, Dan Grossman, Tugkan Batu</li>
+</ul>
+
+</div>
+<?php include("foot.php") ?>
