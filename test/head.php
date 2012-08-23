d7ccdc67100cb78de10e051742eddbabfe9768f5
Mark Reitblatt <reitblatt@cs.cornell.edu>
2012-08-23 10:15:37 +0200

    testing link in header

--- test/head.php
+++ test/head.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>CS Czars</title>
+<title><a href="index.php">Computer Science Dept. Czars</a></title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link type="text/css" rel="stylesheet" href="style.css" />
