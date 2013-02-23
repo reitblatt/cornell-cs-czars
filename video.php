d61a542b521b2590f3163317b3f533bb442c0583
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-02-23 15:55:35 -0500

    forgotten closing tag in video.php

--- video.php
+++ video.php
@@ -3,7 +3,7 @@
 <h1>Videotaping Czar</h1>
 
 <h2>Description</h2>
-<p>The Videotaping Czars are responsible for videotaping all colloquia and job talks. These typically occur in the fall semester on Thursday afternoons, and in the spring semester on Tuesday and Thursday afternoons.<br><br>The colloquium schedule is at <a href="http://www.cs.cornell.edu/Events/colloquium">www.cs.cornell.edu/Events/colloquium</p>
+   <p>The Videotaping Czars are responsible for videotaping all colloquia and job talks. These typically occur in the fall semester on Thursday afternoons, and in the spring semester on Tuesday and Thursday afternoons.<br><br>The colloquium schedule is at <a href="http://www.cs.cornell.edu/Events/colloquium">www.cs.cornell.edu/Events/colloquium</p></a>
 
 <h2>Current Czar</h2>
 <ul>
