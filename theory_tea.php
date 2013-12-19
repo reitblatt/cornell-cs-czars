e703330d56f007dd2763ff4d120d97850deb3994
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-12-19 14:03:40 -0500

    added theory tea czarship

---
+++ theory_tea.php
@@ -0,0 +1,28 @@
+<?php include("head.php") ?>
+<div class="hideable">
+<h1>Theory Tea Czar</h1>
+
+<h2>Description</h2>
+<p>The theory tea buck stops here. To the theory tea, the czar is benevolent dictator, drill sergeant, logistical extraordinaire, and a thousand other things at once. The czar must care for theory tea as if it were their own child: generously, strictly, without bitterness or anger. The duties of the theory tea czar are:
+
+<ul>
+<li>to schedule theory tea</li>
+<li> to reserve a room of appropriate size </li>
+<li> to work with theory tea participants to choose reading and presentation material</li>
+<li> to schedule presenters</li>
+<li> to ensure that appropriate refreshments are served</li>
+<li> to remove any other obstacles to the success of theory tea, as such arise,</li>
+<li> to maintain the theory tea website </li>
+<li> and most importantly, to foster a collaborative and welcoming spirit</li>
+</ul>
+</p>
+
+<br/><a href="http://www.cs.cornell.edu/w8/~talks">www.cs.cornell.edu/w8/~talks</a>
+
+<h2>Current Czar</h2>
+<ul>
+   <li>Sam Hopkins</li>
+</ul>
+
+</div>
+<?php include("foot.php") ?>
